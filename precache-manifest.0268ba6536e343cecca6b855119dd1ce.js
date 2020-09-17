e671a25a38843a7786b84aaaca764f39148b534c
almeera <almeera518@gmial.com>
2020-09-16 20:24:36 -0400

    Updates

---
+++ precache-manifest.0268ba6536e343cecca6b855119dd1ce.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15fae46c6d75fb9f89a156ff465f94af",
+    "url": "/restaurants-search/index.html"
+  },
+  {
+    "revision": "38701ea8369861576d8d",
+    "url": "/restaurants-search/static/css/main.8eb4d80c.chunk.css"
+  },
+  {
+    "revision": "df47956651f61a78d378",
+    "url": "/restaurants-search/static/js/2.ffd7b730.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/restaurants-search/static/js/2.ffd7b730.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38701ea8369861576d8d",
+    "url": "/restaurants-search/static/js/main.5fa04f44.chunk.js"
+  },
+  {
+    "revision": "3536f2c009db63ddb4b4",
+    "url": "/restaurants-search/static/js/runtime-main.5d24d303.js"
+  },
+  {
+    "revision": "863a1265995f08cdb5f21efc22803b23",
+    "url": "/restaurants-search/static/media/background_search_desktop.863a1265.png"
+  },
+  {
+    "revision": "80db980ecd692fb0395f0029a7286d87",
+    "url": "/restaurants-search/static/media/background_search_mobile.80db980e.png"
+  }
+]);
